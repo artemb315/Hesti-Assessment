2822c878c06b9dc8b1f15fda68c8470dd85ab249
Artem Baranov <artembarbanov50@gmail.com>
2024-09-02 17:22:22 +0000

    add responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,26 @@ import ManagementContainer from "./containers/ManagementContainer";
 function App() {
   return (
     <Grid container spacing={0} sx={{ height: "100vh" }}>
-      <Grid item xs={6} sx={{ height: "100%" }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        lg={6}
+        sx={{
+          height: { xs: "50vh", md: "100vh" },
+        }}
+      >
         <MapContainer />
       </Grid>
-      <Grid item xs={6} sx={{ height: "100%" }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        lg={6}
+        sx={{
+          height: { xs: "50vh", md: "100vh" },
+        }}
+      >
         <ManagementContainer />
       </Grid>
     </Grid>
