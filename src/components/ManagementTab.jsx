817bf0d39c4d8d6d7be0197de683adef564272ce
Artem Baranov <artembarbanov50@gmail.com>
2024-09-04 08:28:30 +0000

    fix top bar navigation

--- src/components/ManagementTab.jsx
+++ src/components/ManagementTab.jsx
@@ -1,17 +1,8 @@
-import {
-  POLYGON_MANAGEMENT_TABLE,
-  MARKER_MANAGEMENT_TABLE,
-} from "../constants";
-
 const ManagementTab = ({ text, id, tableType, setTableType }) => {
   const isSelected = id === tableType;
 
   const handleClick = () => {
-    setTableType(
-      tableType === POLYGON_MANAGEMENT_TABLE
-        ? MARKER_MANAGEMENT_TABLE
-        : POLYGON_MANAGEMENT_TABLE,
-    );
+    setTableType(id);
   };
 
   return (
