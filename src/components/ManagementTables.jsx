0cb83af5e1d566335f86dfb1dff2b0a2b0be6071
Artem Baranov <artembarbanov50@gmail.com>
2024-09-05 11:59:02 +0000

    update top bar change

--- src/components/ManagementTables.jsx
+++ src/components/ManagementTables.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import ManagementTopBar from "./ManagementTopBar";
 import PolygonsManagmentTable from "./PolygonsManagmentTable";
 import MarkersManagmentTable from "./MarkersManagmentTable";
 
-import { POLYGON_MANAGEMENT_TABLE } from "../constants";
+import { setStatus } from "../store/slices/globalSlice";
+import {
+  resetEditingPolygon,
+  resetPolygon,
+} from "../store/slices/polygonSlice";
+import { resetEditingMarker, resetMarker } from "../store/slices/markerSlice";
+import { NORMAL_STATUS, POLYGON_MANAGEMENT_TABLE } from "../constants";
 
 const ManagementTables = () => {
   const [tableType, setTableType] = useState(POLYGON_MANAGEMENT_TABLE);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(resetEditingPolygon());
+    dispatch(resetPolygon());
+    dispatch(resetEditingMarker());
+    dispatch(resetMarker());
+    dispatch(setStatus(NORMAL_STATUS));
+  }, [tableType, dispatch]);
 
   return (
     <div className="p-6">
